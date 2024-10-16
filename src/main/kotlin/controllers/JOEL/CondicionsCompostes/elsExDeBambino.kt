3d94b993e7f251b7a7a61d4c47bef0e470d91147
Martí Vilàs <marti.vilas14@gmail.com>
2024-10-16 10:31:24 +0200

    changes on bambino

--- src/main/kotlin/controllers/JOEL/CondicionsCompostes/elsExDeBambino.kt
+++ src/main/kotlin/controllers/JOEL/CondicionsCompostes/elsExDeBambino.kt
@@ -1,5 +1,4 @@
 package controllers.JOEL.CondicionsCompostes
-
 import java.util.Scanner
 
 fun main(){
@@ -14,14 +13,8 @@ fun main(){
     print("Digues n: ")
     val n:Int=scanner.nextInt()
 
-    /**
-     * Cada cas de prova consisteix en 4 números, 𝐴, B, 𝐶, N, sent 𝐴, 𝐵, 𝐶 l'edat dels exs de bambino,
-     * que sempre seran diferents i iguals o majors de 18, i 𝑁 l'edat del seu últim interès,
-     * també igual o major de 18.
-     */
-
-    if(a!=b && b!=c && c!=n) {
-        if (a<=18 && b<=18 && c<=18 && n<=18){
+    if(a!=b && b!=c && a!=c) {
+        if (a>=18 && b>=18 && c>=18 && n>=18){
             println("SI")
         }else{
             println("NO")

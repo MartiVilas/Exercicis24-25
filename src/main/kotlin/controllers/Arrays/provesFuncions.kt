16a3b344c9caa14e7f262b60ddf94ada204ab6e4
Martí Vilàs <marti.vilas14@gmail.com>
2024-11-11 13:33:22 +0100

    create provesFuncions.kt

---
+++ src/main/kotlin/controllers/Arrays/provesFuncions.kt
@@ -0,0 +1,14 @@
+package controllers.Arrays
+
+fun main() {
+
+    val array: Array<Int> = arrayOf(1,2,3,4)
+    val arrayReversed = array.reversed()
+
+    println(array.contentToString())
+    println(arrayReversed.toString())
+
+
+
+
+}

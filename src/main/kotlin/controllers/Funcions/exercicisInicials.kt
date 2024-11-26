bf20f7eab6e669a40e92f3fd19e357a868473bd4
Martí Vilàs <marti.vilas14@gmail.com>
2024-11-26 10:16:53 +0100

    continue exercices functions

--- src/main/kotlin/controllers/Funcions/exercicisInicials.kt
+++ src/main/kotlin/controllers/Funcions/exercicisInicials.kt
@@ -1,8 +1,7 @@
-
 import utils.*
 import kotlin.math.pow
 
-fun main(){
+fun main() {
 
     // Exercici 1
     print("Dime un número para que te de el mismo número al cuadrado: ")
@@ -11,9 +10,9 @@ fun main(){
 
     //Exercici 2
     print("Dime 2 números después te diré si el primero es mayor que el segundo")
-    val num1 : Int = llegirInt()
-    val num2 : Int = llegirInt()
-    println("El primer número es ${majorONo(num1,num2)} que sea mayor que el segundo")
+    val num1: Int = llegirInt()
+    val num2: Int = llegirInt()
+    println("El primer número es ${majorONo(num1, num2)} que sea mayor que el segundo")
 
     //Exercici 3
     print("Dime un radio para una esfera y te calcularé su volumen")
@@ -25,7 +24,7 @@ fun main(){
     print("Num 1: ")
     val num1Calc: Int = llegirInt()
     val num2Calc: Int = llegirInt()
-    calculadora(num1Calc,num2Calc)
+    calculadora(num1Calc, num2Calc)
 
     //Exercici5
     noNegatiu()
@@ -36,125 +35,129 @@ fun main(){
     //Exercici7
 
 
-
 }
 
-fun pow(num :Double): Double {
+fun pow(num: Double): Double {
     val numSquare: Double = num.pow(2.0)
 
     return numSquare
 }
 
-fun majorONo(num1: Int, num2: Int): Boolean{
+fun majorONo(num1: Int, num2: Int): Boolean {
     var boolean: Boolean = false
-    if (num1>num2){
-        boolean=true
+    if (num1 > num2) {
+        boolean = true
         return boolean
-    }else{
+    } else {
         boolean = false
         return boolean
     }
 }
 
-fun radioEsfera(radi: Double) : Double{
-    var pow: Double= radi
-    val calculRadi: Double = ((4.0/3.0)*Math.PI*(radi.pow(3)))
+fun radioEsfera(radi: Double): Double {
+    var pow: Double = radi
+    val calculRadi: Double = ((4.0 / 3.0) * Math.PI * (radi.pow(3)))
 
     return calculRadi
 }
 
-fun calculadora(num1: Int, num2: Int): Double{
+fun calculadora(num1: Int, num2: Int): Double {
     var res: Double = 0.0
     do {
         var error = true
         println("Benvingut a la calucadora pots fer els següents calculs")
-        println("""
+        println(
+            """
             1. Sumar
             2. Restar
             3. Multiplicar
             4. Dividir
             5. Sortir
-        """.trimIndent())
+        """.trimIndent()
+        )
         print("quina opció vols fer: ")
         val opcioUsuari: Int = llegirInt()
-        if (opcioUsuari in 1..5){
+        if (opcioUsuari in 1..5) {
             error = false
             println("Has escogido la opcion $opcioUsuari")
-        }else{
+        } else {
             error = true
             println("Error: Opción inválida")
         }
-        when(opcioUsuari){
-            1->{
-                res = suma(num1,num2)
+        when (opcioUsuari) {
+            1 -> {
+                res = suma(num1, num2)
             }
-            2->{
-                res = resta(num1,num2)
-            }
-            3->{
 
-                res = mult(num1,num2)
+            2 -> {
+                res = resta(num1, num2)
             }
-            4->{
-                if (num2==0){
+
+            3 -> {
+
+                res = mult(num1, num2)
+            }
+
+            4 -> {
+                if (num2 == 0) {
                     return num1.toDouble()
                 }
-                res = div(num1,num2)
+                res = div(num1, num2)
             }
-            5->{
+
+            5 -> {
                 println("Adéu!")
             }
         }
-    }while(error)
+    } while (error)
     return res
 }
 
-fun suma(num1: Int, num2: Int): Double{
-    var suma: Double =  num1.toDouble()+num2
+fun suma(num1: Int, num2: Int): Double {
+    var suma: Double = num1.toDouble() + num2
     return suma
 }
 
-fun resta(num1: Int,num2: Int): Double{
-    val resta: Double = num1-num2.toDouble()
+fun resta(num1: Int, num2: Int): Double {
+    val resta: Double = num1 - num2.toDouble()
     return resta
 }
 
-fun mult(num1: Int,num2: Int): Double{
-    val mult: Double =  num1.toDouble()*num2.toDouble()
+fun mult(num1: Int, num2: Int): Double {
+    val mult: Double = num1.toDouble() * num2.toDouble()
     return mult
 }
 
-fun div(num1: Int,num2: Int): Double{
-    val div: Double = num1.toDouble()/num2.toDouble()
+fun div(num1: Int, num2: Int): Double {
+    val div: Double = num1.toDouble() / num2.toDouble()
     return div
 }
 
-fun noNegatiu(){
+fun noNegatiu() {
     do {
         var error: Boolean = true
         var num: Int = llegirInt()
-        if (num<0){
+        if (num < 0) {
             println("Error el número ha de ser positivo")
-        }else if (num !in 1..10){
+        } else if (num !in 1..10) {
             println("Error el número ha de estar entre 1 i 10")
-        }else{
+        } else {
             error = false
         }
-    }while (error)
-
+    } while (error)
 
 
 }
 
-fun descompte(num1: Int):Double{
+fun descompte(num1: Int): Double {
     var res: Double = 0.0
     println("Ets VIP?")
     val esVip: Boolean = llegirSioNo()
     if (esVip) {
-         res =  num1*(0.2)
-    }else if (num1>200){
-        res = num1*(0.2)
-    }else{
+        res = num1 * (0.2)
+    } else if (num1 > 200) {
+        res = num1 * (0.2)
+    } else {
         res = num1.toDouble()
     }
     return res

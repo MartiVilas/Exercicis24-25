7f0c732275b2c79cd9d2cd9ad9e753b8e8f193fa
Martí Vilàs <marti.vilas14@gmail.com>
2025-01-13 11:21:01 +0100

    add exercices recursivitat made on class and at home

---
+++ src/main/kotlin/controllers/Recursivitat/recursivitat.kt
@@ -0,0 +1,24 @@
+package Recursivitat
+
+import utils.*
+
+fun main() {
+    val random: Int = (0..100).random()
+    println(random)
+    trobarRandom(random)
+}
+
+fun trobarRandom(numRand: Int) {
+
+    print("Intente endivinar quin número és: ")
+    val opcioUsuari = llegirInt()
+    if (opcioUsuari < numRand) {
+        println("El número es més gran")
+        trobarRandom(numRand)
+    } else if (opcioUsuari > numRand) {
+        println("El número es més petit")
+        trobarRandom(numRand)
+    } else {
+        return println("Correcte el número era $numRand")
+    }
+}
